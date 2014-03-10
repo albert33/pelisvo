642abf726c00fd261ea3de2e7f05fb21ac4cb79a
albert33 <albert.llairo@gmail.com>
2014-03-10 18:12:36 +0100

    Eliminades 3 funcions repetides, creada 1 funcio generica + divisio fitxers JS

---
+++ src/js/init.js
@@ -0,0 +1,16 @@
+﻿$( document ).ready(function() {
+	
+	// Iniciar els buscadors
+	var buscadors = [
+		{fbURL: "generes", divID: "buscador_genere"},
+		{fbURL: "buscador_rapid", divID: "buscador_rapid"},
+		{fbURL: "idiomes", divID: "buscador_idiomes"},
+		{fbURL: "buscador_decada", divID: "buscador_decada"}
+	];
+	
+	for (var i=0; i<buscadors.length; i++){
+		generarBuscadors(baseRef + buscadors[i].fbURL , buscadors[i].divID);
+	}
+	
+	
+});
