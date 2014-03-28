a7350ddddf32d7ea41b6dd7dda6baa8fcd5569a8
albert33 <albert.llairo@gmail.com>
2014-03-28 17:32:49 +0100

    Update concatenat JS

--- dest/js/concatenat.js
+++ dest/js/concatenat.js
@@ -2278,18 +2278,37 @@
 
 
 }(window.jQuery);
+/*
+
+TO DO:
+1- Canviar estructura checkboxes (estructura + agafar be els valors)
+2- Fer que funcioni filtrar per decada, idioma, etc.
+3- Ajuntar tots els filtres
+4- Externalitzar la funció per crear el llistat de series
+
+*/
+
+
 var fbRef = "https://pelisvo.firebaseio.com/";
 
 
 function filtrarChecked() {
 	
+	// Borrar llista series (per després tornar-la a escriure filtrada)
+	var div = document.getElementById('mostrar-series');
+	while( div.hasChildNodes() ){
+   	div.removeChild(div.lastChild);
+	}
+	
+	
+	// Agafar valors
 	var cbarray = document.getElementsByTagName("input"); 
 	var valors = [];
 	var buscador = [];
 	
 	// TODO: aconseguir una estructura així dels checkboxes
 	var filtres = {
-  	"buscador_genere": ["Història", "Altres"]
+  	"buscador_genere": ["Història", "Crim"]
 	};
 	
 	for (var i = 0; i < cbarray.length; i++) {
@@ -2300,20 +2319,9 @@ function filtrarChecked() {
 		} 
 	}
 	
-	for (var i = 0; i<valors.length; i++){
-		console.log(buscador[i]+" : "+valors[i]);
-		
-	}
-	
+	//Filtrar per genere
 	cercaPerGenere(filtres.buscador_genere);
-	/*
-	
-	Cridar a la funció vistaLlistaSeries,
-	passant-li els filtres com a paràmetres
-	per a que torni a carregar les series,
-	però que només carreguin les que ha filtrat l'usuari
-	
-	*/
+
 	
 }
 
@@ -2333,7 +2341,7 @@ function cercaPerGenere(generesFiltre) {
             if (generesSerie.indexOf(item) != -1 &&
                !matching.hasOwnProperty(csnap.name())) {
                 console.log("Matching! Serie " + serie.titol_es + " genere " + item);
-              matching[csnap.name()] = serie;
+              	matching[csnap.name()] = serie;
             }
           });
           
@@ -2341,11 +2349,87 @@ function cercaPerGenere(generesFiltre) {
       }
     });
     console.log(matching);
+   filtratPerGeneres(matching);
     // ja les tens, ara has de tornar a actualitzar la vista
     // per això és important separar les consultes al back-end de la manipulació del DOM
     // ara podries aprofitar gran part de vistaLlistaSeries si ho treus en una funció externa
     
     // no serà fàcil ajuntar tots els filtres de diversos conceptes (any, idioma).
+}
+
+
+function filtratPerGeneres(matching){
+
+	var div = document.getElementById('mostrar-series');
+   var llista = document.createElement("ul");
+   llista.className = "llista-resultats-item";
+   div.appendChild(llista);
+   
+	for (var atribut in matching){
+		var objSerie = matching[atribut];
+      
+      // Cada serie és un <li>
+				var serie = document.createElement("li");
+				serie.className = "caixa-serie-item";
+				llista.appendChild(serie);
+				
+					// Div de la caratula
+					var divCaratula = document.createElement("div");
+					divCaratula.className = "llista-resultats-caratula";
+					serie.appendChild(divCaratula);
+				
+					// Caratula enllaç <a>
+					var aCaratula = document.createElement("a");
+					aCaratula.className = "pull-left";
+					aCaratula.href = "";
+					divCaratula.appendChild(aCaratula);
+						
+					// Imatge caratula <img>
+					var imgCaratula = document.createElement("img");
+					imgCaratula.className = "caratula-llista";
+					imgCaratula.src = objSerie.caratula;
+					aCaratula.appendChild(imgCaratula);
+					
+					// Div gran informació
+					var divGranInfo = document.createElement("div");
+					divGranInfo.className = "llista-contenidor-item";
+					serie.appendChild(divGranInfo);
+					
+					// Títol <a>
+					var aTitol = document.createElement("a");
+					aTitol.className = "llista-titol";
+					aTitol.href = "";
+					var titol_es = document.createTextNode(objSerie.titol_es);
+					aTitol.appendChild(titol_es);
+					divGranInfo.appendChild(aTitol);
+					
+					// Div info basica segona linia
+					var divInfoBasica = document.createElement("div");
+					divInfoBasica.className = "llista-info-basica";
+					
+					// No esta acabat. Falten els espais en blanc <&nbsp;>
+					var segonaLinia = document.createTextNode(objSerie.any_estrena+" | "+objSerie.puntuacio_global+" | "+objSerie.generes);
+					divInfoBasica.appendChild(segonaLinia);
+					divGranInfo.appendChild(divInfoBasica);
+						
+						//Div sinopsis
+						var divSinopsis = document.createElement("div");
+						divSinopsis.className = "llista-sinopsis";
+						var textSinopsis = document.createTextNode(objSerie.sinopsis);
+						divSinopsis.appendChild(textSinopsis);
+            divGranInfo.appendChild(divSinopsis);
+						
+						//Div info extra ultima linia
+						var divInfoExtra = document.createElement("div");
+						divInfoExtra.className = "llista-info-extra";
+						var ultimaLinia = document.createTextNode("Temporades: "+objSerie.temporades+" | Capítols totals: "+objSerie.capitols_totals+" | Durada: "+objSerie.durada_episodis+" min.");
+						divInfoExtra.appendChild(ultimaLinia);
+					divGranInfo.appendChild(divInfoExtra);
+		
+		
+		
+	}
+	
 }
 
 
